2779dd59c6fbad1c2e39ef8ac486e37a1caf341c
Sylva Zhang <z.zhang.9630@westcliff.edu>
2026-08-02 22:43:45 -0500

    add js activity

--- js/activity.js
+++ js/activity.js
@@ -1,19 +1,15 @@
 $(document).ready(function () {
 
-    $("table tbody tr").each(function () {
+    $("#activities table tbody tr").each(function () {
 
         $(this).find("td").not(":first").each(function () {
 
-            const activityCell = $(this);
-            const activityText = activityCell.text().trim();
+            var activityCell = $(this);
+            var activityName = activityCell.text().trim();
 
-            if (activityText !== "Not Available") {
+            if (activityName !== "Not Available") {
 
                 activityCell.addClass("selectable");
-
-                activityCell.on("click", function () {
-                    $(this).toggleClass("selected");
-                });
 
             } else {
 
@@ -22,6 +18,61 @@ $(document).ready(function () {
             }
 
         });
+
+    });
+
+    $("#activities table tbody td.selectable").click(function () {
+
+        var selectedCell = $(this);
+
+        var activityName = selectedCell.text().trim();
+
+        var columnIndex = selectedCell.index();
+
+        var cliffName = $("#activities table thead th")
+            .eq(columnIndex)
+            .text()
+            .trim();
+
+        var rowIndex = selectedCell.parent().index();
+
+        var activityKey = rowIndex + "-" + columnIndex;
+
+        selectedCell.toggleClass("selected");
+
+        if (selectedCell.hasClass("selected")) {
+
+            $("#displaySelected").css({
+                "visibility": "visible",
+                "margin-top": "2em"
+            });
+
+            $("#result").append(
+                "<p data-activity='" +
+                activityKey +
+                "'>" +
+                activityName +
+                " at <span>" +
+                cliffName +
+                "</span></p>"
+            );
+
+        } else {
+
+            $("#result p[data-activity='" +
+                activityKey +
+                "']").remove();
+
+            if ($("#result p").length === 0) {
+
+                $("#displaySelected").css({
+                    "visibility": "hidden",
+                    "margin-top": "0"
+                });
+
+            }
+
+        }
 
     });
 
